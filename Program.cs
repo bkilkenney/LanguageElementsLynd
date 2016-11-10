f31d44bf391d07d2a9c1dd66698e5ad7329f2699
Brad Kilkenney <bkilkenney@yahoo.com>
2016-11-10 17:57:54 -0500

    Added method

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace LanguageElementsLynda
     {
         static void Main(string[] args)
         {
+
+            //ConcatTwoStrings("First string ", "and this is the second/last string");
+
+            string mySentence = ConcatTwoStrings("First string and ", "last string.");
+            Console.WriteLine(mySentence);
         }
+
+        static string ConcatTwoStrings(string str1, string str2)
+        {
+            string fullString = str1 + str2;
+            return fullString;            
+        }
+
+
     }
 }
